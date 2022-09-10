1706cca9f0e00e238a5fa890bc4e6712ab27f357
Anzhela Ilieva <anil@html24.net>
2022-09-10 15:45:20 +0300

    TH-1 Initiating the sidebar

---
+++ src/components/Main/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Map from "../../../Map/Map";
+
+export default function Home() {
+  return (
+    <>
+      <Map latitude={51.505} longtitude={-0.09} />
+    </>
+  );
+}
---
+++ src/components/Sidebar/SidebarData.js
@@ -0,0 +1,19 @@
+export const SidebarData = [
+  {
+    title: "Home",
+    icon: "",
+    path: "/",
+  },
+
+  {
+    title: "Line",
+    icon: "",
+    path: "/line",
+  },
+
+  {
+    title: "Table",
+    icon: "",
+    path: "/table",
+  },
+];
