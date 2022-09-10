74fcc7727758319f2899eee728e6fa0af3382350
Anzhela Ilieva <anil@html24.net>
2022-09-10 10:51:23 +0300

    TH-2 Creating the Map component and fetching the data from Firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Map from "./components/Map/Map";
 
-function App() {
+export default function App() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      <Map latitude={51.505} longtitude={-0.09} />
+    </>
   );
 }
-
-export default App;
